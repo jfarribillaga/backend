ea25982a200e9c0938d149ac01ef5f086d0c560b
Juan Arribillaga <juan.arribillaga@globant.com>
2016-08-08 18:47:17 +0100

    Added / endpoint and console.log feedback.

--- api/index.js
+++ api/index.js
@@ -6,6 +6,11 @@ const topicsObj = require('../data/retrieveTopics001.json');
 const router = require('express').Router();
 const fs = require('fs');
 
+router.use(function timeLog(req, res, next) {
+  console.log('Endpoint: ' + req.rawHeaders[1] + req.url);
+  next();
+});
+
 router.use('/appointmentList', function getAppointments(req, res){
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(appointmentListObj))
@@ -21,5 +26,13 @@ router.use('/topics', function getLocations(req, res) {
   res.send(JSON.stringify(topicsObj)) 
 });
 
+router.use('/', function getAppointments(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({"endpoints":[
+    'appointmentList',
+    'locations',
+    'topics'
+  ]}))
+});
 
 module.exports = router;

0e64cf72e38267762f7ff3d236bc70d35c2c914a
Juan Arribillaga <juan@letfoo.com>
2017-01-07 19:41:18 -0300

    Changed endpoints.

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const appointmentListObj = require('../data/appointmentList001.json');
-const appointmentBookedList = [
-  require('../data/appointmentBooked01.json'),
-  require('../data/appointmentBooked02.json')
-];
-const locationsListObj = require('../data/retrieveLocations_404362.json');
-const topicsObj = require('../data/retrieveTopics001.json');
+
+const states = require('../mocks/states.json');
+const types = require('../mocks/types.json');
+const dashboard = require('../mocks/dashboard.json');
+
+
 const router = require('express').Router();
 const fs = require('fs');
 const utils = require('../utils/utils.js');
@@ -16,24 +15,19 @@ router.use((req, res, next) => {
   next();
 });
 
-router.use('/appointmentList', (req, res) => {
+router.use('/states', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(appointmentListObj))
+  res.send(JSON.stringify(states))
 });
 
-router.use('/appointmentBooked', (req, res) => {
+router.use('/types', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(appointmentBookedList[utils.getRandomInt(0, appointmentBookedList.length)]));
+  res.send(JSON.stringify(types));
 });
 
-router.use('/locations', (req, res) => {
+router.use('/dashboard', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(locationsListObj)) 
-});
-
-router.use('/topics', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(topicsObj)) 
+  res.send(JSON.stringify(dashboard)) 
 });
 
 router.use('/', (req, res) => {
@@ -41,10 +35,9 @@ router.use('/', (req, res) => {
   res.send(JSON.stringify({
     "host": req.rawHeaders[1],
     "endpoints":[
-      '/appointmentList',
-      '/appointmentBooked',
-      '/locations',
-      '/topics'
+      '/states',
+      '/types',
+      '/dashboard'
   ]}))
 });
 
